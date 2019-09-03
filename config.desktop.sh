0ab23f40f2a68e35e55bcf9b964ddf331a9193a3
hiro98 <hiro@protagon.space>
2019-09-03 21:02:45 +0200

    some mods

--- config.desktop.sh
+++ config.desktop.sh
@@ -3,9 +3,8 @@ ETH_DEV="enp3s0"
 
 MAIN_NET=$ETH_DEV
 MAIN_FONT="Source Code Pro 8"
-MAIN_MONITOR=DisplayPort-0
-MONITORS=(DVI-I-0 DisplayPort-2)
-SCREENLAYOUT="xrandr --output DisplayPort-1 --off --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-2 --mode 1920x1080 --pos 3840x0 --rotate normal --output DVI-I-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-1 --off"
+MAIN_MONITOR=DisplayPort-2
+MONITORS=(DVI-I-0 DisplayPort-1)
 RYGEL="true"
 BAR_POS="bottom"
 DPI=96
