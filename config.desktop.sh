e4e04bc7cf820cb0663fdb1597dfe047f512da5b
Valentin Boettcher <hiro@protagon.space>
2020-12-31 18:23:06 +0100

    make bspwm config ready for the main desktop

--- config.desktop.sh
+++ config.desktop.sh
@@ -3,8 +3,9 @@ ETH_DEV="enp3s0"
 
 MAIN_NET=$ETH_DEV
 MAIN_FONT="Source Code Pro 8"
-MAIN_MONITOR=DisplayPort-0
-MONITORS=(DVI-I-0 DisplayPort-2)
+MONITORS=($(xrandr | rg -i "\sconnected" | rg -v eDP | cut -d" " -f1))
+MAIN_MONITOR=${MONITORS[2]}
+SIDE_MONITORS=($(xrandr | rg -i "\sconnected" | rg -v eDP | cut -d" " -f1 | grep -v $MAIN_MONITOR))
 RYGEL="true"
 BAR_POS="bottom"
 DPI=91
