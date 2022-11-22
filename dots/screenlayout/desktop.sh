6c89e31e333ab1346a94ff771c935955d0c8d722
Valentin Boettcher <hiro@protagon.space>
2022-11-22 18:02:20 +0100

    implement scaling

--- dots/screenlayout/desktop.sh
+++ dots/screenlayout/desktop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 5760x1080 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --mode 3840x2160 --pos 1920x0 --rotate normal --rate 59.94 --output DVI-D-0 --off
-xrandr --dpi 110
-xrandr --dpi 110
+xrandr --output DisplayPort-0 --mode 3840x2160 --pos 3840x0 --rotate normal --output DisplayPort-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off
+xrandr --dpi 157
+xrandr --dpi 157
